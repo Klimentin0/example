c2f822079d3cdb11703b06495bf758cd28db69e8
Klimentin0 <meshkov.klim@gmail.com>
2024-10-27 19:12:40 +0300

    Верстка форм регистрации и логина

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -9,8 +11,12 @@ Route::resource('jobs', JobController::class);
 
 Route::view('/contact', 'contact');
 
+//Auth
+Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
 
-
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
 //old Route for home
 //Route::get('/', function () {
 //
